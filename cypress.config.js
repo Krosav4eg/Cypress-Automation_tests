5fc7bac4525cbdfd1e53f1338062c720e877c93e
Sergey Potapov <sergeypotapoff@12gmail.com>
2023-05-22 18:47:48 +0300

    was added slack integration and mocha report

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
     e2e: {
         viewportWidth: 1200,
         viewportHeight: 800,
-        retries: 3
+        retries: 3,
+        mochawesomeReporterOptions: {
+            reportDir: "cypress/report/mochawesome-report/repo",
+            overwrite: true,
+            html: true,
+            json: true,
+            timestamp: "mmddyyyy_HHMMss"
+        },
+        screenshotsFolder: "cypress/report/mochawesome-report/screen",
     },
 });
